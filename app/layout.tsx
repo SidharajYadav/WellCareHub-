f66bc433c55ad753348272be9b3a365608ecc3ec
SiddharajYadav <sidharajyadav157@gmail.com>
2024-08-19 22:20:35 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
     "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
   icons: {
     icon: "/assets/icons/logo-icon.svg",
-  },
+  }, 
 };
 
 export default function RootLayout({
