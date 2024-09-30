d06d186946af03d10202f56017a29e7cdd2cc8c9
SiddharajYadav <sidharajyadav157@gmail.com>
2024-09-30 11:15:01 +0530

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -4,7 +4,7 @@ declare type SearchParamProps = {
   params: { [key: string]: string };
   searchParams: { [key: string]: string | string[] | undefined };
 };
-
+ 
 declare type Gender = "male" | "female" | "other";
 declare type Status = "pending" | "scheduled" | "cancelled";
 
